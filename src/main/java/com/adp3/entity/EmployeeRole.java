299b7efd5429b7bec4271460823008aab5ac11f6
solly <214293939@mycput.ac.za>
2020-07-04 20:38:14 +0200

    New class added, employeeRole

---
+++ src/main/java/com/adp3/entity/EmployeeRole.java
@@ -0,0 +1,52 @@
+package com.adp3.entity;
+
+public class EmployeeRole {
+    private String empId, roleId;
+
+    private EmployeeRole(Builder builder){
+        this.empId = builder.empId;
+        this.roleId = builder.roleId;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeRole{" +
+                "empId='" + empId + '\'' +
+                ", roleId='" + roleId + '\'' +
+                '}';
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public static class Builder{
+        private String empId, roleId;
+
+        public Builder setEmployeeId(String empId){
+            this.empId = empId;
+            return this;
+        }
+
+        public Builder setRoleId(String roleId){
+            this.roleId = roleId;
+            return this;
+        }
+
+        public Builder copy(EmployeeRole employeeR){
+            this.empId = employeeR.empId;
+            this.roleId = employeeR.roleId;
+            return  this;
+        }
+
+        public  EmployeeRole build(){
+            return new  EmployeeRole(this);
+        }
+
+    }
+
+}
