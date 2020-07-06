4133ca2cce1b625e9385088d322a231e4400d98f
zubairvo <zubairvo@gmail.com>
2020-07-06 11:12:23 +0200

    Employee Class Created, Leave Updated

--- src/main/java/com/adp3/entity/EmployeeLeave.java
+++ src/main/java/com/adp3/entity/EmployeeLeave.java
@@ -1,11 +1,102 @@
 package com.adp3.entity;
 
 /**
- * Author: Zubair Van Oudtshoorn
- * Class: Part Time
- * Student number: 217203795
- * Class Description: EmployeeLeave
- */
+ * @Zubair Van Oudtshoorn
+ * Createion of EmployeeLeave Class
+ *
+ * **/
 
 public class EmployeeLeave {
+
+    private int employee_id, leaveID;
+    private String startDate, endDate;
+
+
+    private EmployeeLeave(Builder builder){
+        this.employee_id = builder.employee_id;
+        this.leaveID = builder.leaveID;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+    }
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public int getLeaveID() {
+        return leaveID;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee Leave{" +
+                "Employee ='" + employee_id + '\'' +
+                ", Leave Type ='" + leaveID + '\'' +
+                ", Start date ='" + startDate + '\'' +
+                ", End date ='" + endDate + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int employee_id, leaveID;
+        private String startDate, endDate;
+
+
+        /*  public Builder(int employee_id, int leaveID, String startDate, String endDate) {
+              this.employee_id = employee_id;
+              this.leaveID = leaveID;
+              this.startDate = startDate;
+              this.endDate = endDate;
+          }
+          */
+
+
+        public EmployeeLeave.Builder setEmployee_id(int employee_id) {
+            this.employee_id = employee_id;
+            return this;
+        }
+
+        public EmployeeLeave.Builder setLeaveID(int leaveID) {
+            this.leaveID = leaveID;
+            return this;
+        }
+
+        public EmployeeLeave.Builder setStartDate(String startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public EmployeeLeave.Builder setEndDate(String endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+
+        public EmployeeLeave.Builder copy(EmployeeLeave employeeLeave) {
+
+            this.employee_id = employeeLeave.employee_id;
+            this.leaveID = employeeLeave.leaveID;
+            this.startDate = employeeLeave.startDate;
+            this.endDate = employeeLeave.endDate;
+
+            return this;
+
+
+        }
+
+        public EmployeeLeave build() {
+
+            return new EmployeeLeave(this);
+        }
+    }
+
+
+
 }
