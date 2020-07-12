4a035bf107c4981ab4f1cd80a447126bb758058b
Megan Jacobs <megan.jacobs@outlook.com>
2020-07-12 17:13:04 +0200

    LeaveReportFactory updated with comments

--- src/main/java/com/adp3/factory/LeaveReportFactory.java
+++ src/main/java/com/adp3/factory/LeaveReportFactory.java
@@ -3,11 +3,20 @@ package com.adp3.factory;
 import com.adp3.entity.LeaveReport;
 import com.adp3.util.GenericHelper;
 
+/**
+ * Author: Megan Jacobs
+ * Class: Part Time
+ * Student number: 211137162
+ * Factory: LeaveReportFactory
+ */
+
 public class LeaveReportFactory {
 
-    public static LeaveReport getLeaveReport (String leaveReportID,String leaveID,String leaveReportDesc) {
+    /* general method for user to retrieve a LeaveReport,
+    ** receives 2 values received from user and auto-generates leaveReportID*/
+    public static LeaveReport getLeaveReport (String leaveID,String leaveReportDesc) {
         return new LeaveReport.Builder()
-                    .setLeaveReportID(leaveReportID)
+                    .setLeaveReportID(GenericHelper.generateID())
                     .setLeaveID(leaveID)
                     .setLeaveReportDesc(leaveReportDesc)
                     .build();
