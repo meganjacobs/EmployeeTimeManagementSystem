747c66524e7045322e096035e2e27cb8ac70759d
Megan Jacobs <megan.jacobs@outlook.com>
2020-07-12 16:43:02 +0200

    factory package added to test folder

---
+++ src/test/java/com/adp3/factory/LeaveReportFactoryTest.java
@@ -0,0 +1,16 @@
+package com.adp3.factory;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+
+public class LeaveReportFactoryTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+
+    }
+}
