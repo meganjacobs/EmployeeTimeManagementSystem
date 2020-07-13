e4ce877a6c6354df721de37160ef1a638934a293
solly <214293939@mycput.ac.za>
2020-07-13 23:03:27 +0200

    Factory test class created with test method of getEmployeeRole and a method to check two objects created are not the same.

--- src/test/java/com/adp3/factory/EmployeeRoleFactoryTest.java
+++ src/test/java/com/adp3/factory/EmployeeRoleFactoryTest.java
@@ -1,10 +1,11 @@
 package com.adp3.factory;
 
 import com.adp3.entity.EmployeeRole;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Author: Sonwabo Kasi
  * Class: Part Time
@@ -12,43 +13,39 @@ import org.junit.Test;
  * Class Description: EmployeeRoleFactory Test
  */
 
-import static org.junit.Assert.assertEquals;
-
 public class EmployeeRoleFactoryTest {
 
     public static void beforeClass() throws Exception {
-        EmployeeRole empR = EmployeeRoleFactory.getEmployeeRole("1001M","Manager");
+        EmployeeRole empR = EmployeeRoleFactory.getEmployeeRole("1001M", "Manager");
 
 
     }
 
-
-
     @Test
     public void getEmployeeRole() {
-//Testing object is sets values inserted
-          EmployeeRole empR= new EmployeeRole.Builder()
-                    .setEmployeeId("1002G")
-                    .setRoleId("General")
-                    .build();
-            assertEquals("1002G" + "General",empR.getEmpID() + empR.getRoleID());
-            System.out.println(empR.toString());
+        //Testing object is sets values inserted
+        EmployeeRole empR = new EmployeeRole.Builder()
+                .setEmployeeId("1002G")
+                .setRoleId("General")
+                .build();
+        assertEquals("1002G" + "General", empR.getEmpID() + empR.getRoleID());
+        System.out.println(empR.toString());
 
     }
 
     @Test
     public void multipleObjects() {
         //Testing object are not the same
-        EmployeeRole empR2= new EmployeeRole.Builder()
+        EmployeeRole empR2 = new EmployeeRole.Builder()
                 .setEmployeeId("1001M")
                 .setRoleId("Manager")
                 .build();
-        EmployeeRole empR1= new EmployeeRole.Builder()
+        EmployeeRole empR1 = new EmployeeRole.Builder()
                 .setEmployeeId("1002G")
                 .setRoleId("General")
                 .build();
 
-        Assert.assertNotSame(empR1,empR2.toString());
+        Assert.assertNotSame(empR1, empR2.toString());
         System.out.println(empR1);
         System.out.println(empR2);
 
