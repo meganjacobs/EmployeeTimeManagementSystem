69e9562e1fbb402511cf32014d3374f6f96d5952
zubairvo <zubairvo@gmail.com>
2020-07-13 23:24:05 +0200

    EmployeeLeaveFactoryCreated

--- src/main/java/com/adp3/factory/EmployeeLeaveFactory.java
+++ src/main/java/com/adp3/factory/EmployeeLeaveFactory.java
@@ -1,4 +1,22 @@
 package com.adp3.factory;
 
+
+import com.adp3.entity.EmployeeLeave;
+
+/**
+ * @author Zubair Van Oudtshoorn
+ * desc: Employess Leave Factory - Calculate amount of Leave Days taken by a Employee
+ */
 public class EmployeeLeaveFactory {
+
+    public  static EmployeeLeave calcEmployeeLeave(String empID, String leaveID, String startDate, String endDate){
+        return new EmployeeLeave.Builder()
+                .setEmpID(empID)
+                .setLeaveID(leaveID)
+                .setStartDate(startDate)
+                .setEndDate(endDate)
+                .build();
+
+
+    }
 }
--- src/test/java/com/adp3/factory/EmployeeLeaveFactoryTest.java
+++ src/test/java/com/adp3/factory/EmployeeLeaveFactoryTest.java
@@ -1,5 +1,32 @@
+package com.adp3.factory;
+
+import com.adp3.entity.EmployeeLeave;
+import org.junit.Assert;
+import org.junit.Test;
+/*
+* Zubair Van Oudtshoorn
+*
+* Unit Tests
+* */
 import static org.junit.Assert.*;
 
 public class EmployeeLeaveFactoryTest {
+    //create Object , assign values
+    EmployeeLeave employeeLeave = EmployeeLeaveFactory.calcEmployeeLeave("A16", "A1", "21/05/2020", "28/05/2020");
 
+
+    @Test
+    public void leaveIDNotNull() {
+        // Test leaveID is not null
+        Assert.assertNotNull(employeeLeave.getLeaveID());
+    }
+    @Test
+    public void getEmployeeLeaveValues(){
+        //Test all values are being received
+        assertSame("A16",employeeLeave.getEmpID());
+        Assert.assertEquals(employeeLeave.getLeaveID(), "A1");
+        assertSame("21/05/2020", employeeLeave.getStartDate());
+        assertSame("28/05/2020", employeeLeave.getEndDate());
+
+    }
 }
