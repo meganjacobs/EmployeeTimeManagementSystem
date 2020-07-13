48bea5350052c89f9729f91d4695edb2724fbbbc
Megan Jacobs <megan.jacobs@outlook.com>
2020-07-13 18:29:39 +0200

    individual tests broken up

--- src/test/java/com/adp3/factory/LeaveReportFactoryTest.java
+++ src/test/java/com/adp3/factory/LeaveReportFactoryTest.java
@@ -1,10 +1,6 @@
 package com.adp3.factory;
 
-import com.adp3.entity.Employee;
-import com.adp3.entity.Leave;
 import com.adp3.entity.LeaveReport;
-import com.adp3.util.GenericHelper;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,16 +18,21 @@ public class LeaveReportFactoryTest {
     @Before
     public void setUp() throws Exception {
         this.leaveReport = new LeaveReport.Builder().build();
+        leaveReport = LeaveReportFactory.createLeaveReport("001","Sick Leave");
     }
     @Test
     public void testCreateLeaveReport() {
-        leaveReport = LeaveReportFactory.createLeaveReport("001","Sick Leave");
-        // Test leaveReportID is not null
-        Assert.assertNotNull(leaveReport.getLeaveReportID());
-
         //Test object does not return nulls
         Assert.assertNotNull(leaveReport);
+    }
 
+    @Test
+    public void testLeaveReportIDNotNull() {
+        // Test leaveReportID is not null
+        Assert.assertNotNull(leaveReport.getLeaveReportID());
+    }
+    @Test
+    public void testLeaveDoesNotReturnNull() {
         //Test object values are equal
         Assert.assertEquals(leaveReport.getLeaveID(), "001");
         Assert.assertEquals(leaveReport.getLeaveReportDesc(),"Sick Leave");
