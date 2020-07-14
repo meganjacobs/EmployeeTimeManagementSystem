476d76ff9151d7b26d6162a7650bc91ed877393e
SteveZangwa <61194558+SteveZangwa@users.noreply.github.com>
2020-07-15 00:08:25 +0200

    StoreReportFactoryTest creacted

--- src/test/java/com/adp3/factory/StoreReportsFactoryTest.java
+++ src/test/java/com/adp3/factory/StoreReportsFactoryTest.java
@@ -1,22 +1,31 @@
+/**
+ * Author: Steve Zangwa
+ * Class: Part Time
+ * Student number: 217136664
+ * Factory: StoreReportsFactoryTest
+ */
+
+
 package com.adp3.factory;
 
-import org.junit.After;
-import org.junit.Before;
+import com.adp3.entity.StoreReports;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class StoreReportsFactoryTest {
 
-    @Before
-    public void setUp() throws Exception {
+    StoreReports storeReports = StoreReportsFactory.createStoreReports("23","Admin");
+
+     @Test
+    public void getStoreReports() throws Exception {
+
+        Assert.assertNotNull(storeReports);
+        Assert.assertNotNull(storeReports.getReportID());
+        Assert.assertEquals(storeReports.getStoreID(),"23");
+        Assert.assertEquals(storeReports.getReportDesc(),"Admin");
+
+         System.out.println(storeReports);
+
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void createStoreReports() {
-    }
 }
