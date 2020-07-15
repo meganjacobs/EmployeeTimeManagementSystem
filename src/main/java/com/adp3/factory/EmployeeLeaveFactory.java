f21bc094517ea961fb9a95ba3b1b811b355dbb2c
zubairvo <zubairvo@gmail.com>
2020-07-15 09:07:37 +0200

    Added Comments

--- src/main/java/com/adp3/factory/EmployeeLeaveFactory.java
+++ src/main/java/com/adp3/factory/EmployeeLeaveFactory.java
@@ -7,7 +7,14 @@ import com.adp3.entity.EmployeeLeave;
  * @author Zubair Van Oudtshoorn
  * desc: Employess Leave Factory - Calculate amount of Leave Days taken by a Employee
  */
+
 public class EmployeeLeaveFactory {
+
+    /*method used to create a new EmployeeLeave
+     * @param: EmpID - eg. Emp A16, Emp A1 etc & LeaveID pulled from LeaveFactory
+     *  - eg. Annual Leave, Sick Leave etc as well as start and end date to be received from user
+     * @return: set Values
+     * */
 
     public  static EmployeeLeave calcEmployeeLeave(String empID, String leaveID, String startDate, String endDate){
         return new EmployeeLeave.Builder()
