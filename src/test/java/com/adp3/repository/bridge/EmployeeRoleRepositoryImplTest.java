80691aa3374b9f8de2cc78a8ffcd747b90dc2a07
solly <214293939@mycput.ac.za>
2020-09-02 13:33:58 +0200

    EmployeeRole repository created, with test cases and an interface(updated and errors fixed). Method in factory refactured from getEmpRole to create.EmpRole.

--- src/test/java/com/adp3/repository/bridge/EmployeeRoleRepositoryImplTest.java
+++ src/test/java/com/adp3/repository/bridge/EmployeeRoleRepositoryImplTest.java
@@ -20,12 +20,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeRoleRepositoryImplTest {
 
     private static EmployeeRoleRepository employeeRoleRepository = new EmployeeRoleRepositoryImpl();
-    private EmployeeRole employeeRole= EmployeeRoleFactory.createEmployeeRole("101","Manageer");
+    private EmployeeRole employeeRole = EmployeeRoleFactory.createEmployeeRole("101", "Manageer");
 
     @Test
     void a_create() {
         EmployeeRole created = employeeRoleRepository.create(employeeRole);
-        assertEquals(employeeRole,created);
+        assertEquals(employeeRole, created);
         System.out.println(created);
 
     }
@@ -34,11 +34,10 @@ class EmployeeRoleRepositoryImplTest {
     void b_read() {
         EmployeeRole read = employeeRoleRepository.read(employeeRole.getEmpID());
         System.out.println(employeeRole);
-
     }
 
     @Test
-    void c_update() {
+    void update() {
         EmployeeRole updated = new EmployeeRole.Builder().copy(employeeRole).setEmployeeId("101").setRoleId("Manager").build();
         updated = employeeRoleRepository.update(updated);
         System.out.println(updated);
@@ -46,7 +45,7 @@ class EmployeeRoleRepositoryImplTest {
     }
 
     @Test
-    public void e_getAll() {
+    void e_getAll() {
         System.out.println(employeeRoleRepository.getAll());
     }
 
