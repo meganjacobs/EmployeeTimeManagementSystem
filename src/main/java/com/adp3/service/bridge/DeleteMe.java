8124ef14a4fed0c7223c4836c03dea98ff895500
Megan Jacobs <megan.jacobs@outlook.com>
2020-09-06 14:34:01 +0200

    create service package

---
+++ src/main/java/com/adp3/service/bridge/DeleteMe.java
@@ -0,0 +1,4 @@
+package com.adp3.service.bridge;
+
+public interface DeleteMe {
+}
---
+++ src/main/java/com/adp3/service/reports/impl/LeaveReportServiceImpl.java
@@ -0,0 +1,11 @@
+package com.adp3.service.reports.impl;
+
+/**
+ * Author: Megan Jacobs
+ * Class: Part Time
+ * Student number: 211137162
+ * Description: LeaveReportServiceImpl - implementation of LeaveReportService as a concrete class
+ */
+
+public class LeaveReportServiceImpl {
+}
