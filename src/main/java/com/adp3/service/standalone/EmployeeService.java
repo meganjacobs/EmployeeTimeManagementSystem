937379f3b49ee21aa21bc3358f3d51308235794b
Malusi Pakade <pakadem@gmail.com>
2020-09-09 14:43:17 +0200

    Create Employee service interface and implements the interface. Test employee interface implementstion 2

--- src/main/java/com/adp3/service/standalone/EmployeeService.java
+++ src/main/java/com/adp3/service/standalone/EmployeeService.java
@@ -1,4 +1,18 @@
 package com.adp3.service.standalone;
 
-public class EmployeeService {
+import com.adp3.entity.standalone.Employee;
+import com.adp3.service.IService;
+import java.util.Set;
+
+/**
+ * @author Malusi Pakade
+ * 209105917
+ * desc: Employee Service Interface
+ * 2020
+ */
+
+public interface EmployeeService extends IService<Employee, String> {
+
+    Set<Employee> getAll();
+
 }
--- src/test/java/com/adp3/service/standalone/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/adp3/service/standalone/impl/EmployeeServiceImplTest.java
@@ -1,19 +1,85 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+package com.adp3.service.standalone.impl;
+
+import com.adp3.entity.standalone.Employee;
+import com.adp3.factory.standalone.EmployeeFactory;
+import com.adp3.service.standalone.EmployeeService;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
+/**
+ * @author Malusi Pakade
+ * 209105917
+ * desc: Employee Service Implementation Test
+ * 2020
+ */
 
-@RunWith(Arquillian.class)
+/** responsible for executing methods in alphabetical order */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class EmployeeServiceImplTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(com.adp3.service.standalone.impl.EmployeeServiceImpl.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+    private static EmployeeService emp_service = EmployeeServiceImpl.getEmpService();
+    private Employee employee = EmployeeFactory.createEmployee("231","Malusi", "Pakade", "021 543 9876", new Date(1986, 00, 12));
+
+
+    @Test
+    public void getEmp_service() {
+
     }
 
+    @Test
+    public void d_getAll() {
+
+        System.out.print( "Get All: " + emp_service.getAll() );
+
+    }
+
+    @Test
+    public void a_create() {
+
+        // test if EmployeeRepositoryImpl create method can create an employee
+        Employee created = emp_service.create( employee );
+
+        //test expected empID value in employee object.
+        assertEquals( employee.getEmpID(), employee.getEmpID() );
+        //test expected empName value in employee object
+        assertEquals( employee.getEmpName(), employee.getEmpName() );
+        //test expected empLastName value in employee object
+        assertEquals( employee.getEmpLastName(), employee.getEmpLastName() );
+        //test expected empPhone value in employee object
+        assertEquals( employee.getEmpPhone(), employee.getEmpPhone() );
+        //test expected empDOB value in employee object
+        assertEquals( employee.getEmpDOB(), employee.getEmpDOB() );
+
+        System.out.println("Created: " + created );
+
+    }
+
+    @Test
+    public void b_read() {
+
+        Employee read = emp_service.read( employee.getEmpID() );
+        System.out.println("Read: " + read );
+
+    }
+
+    @Test
+    public void c_update() {
+
+        Employee update = new Employee.Builder().copy(employee).setEmpID("400").build() ;
+        update = emp_service.update( update );
+        System.out.println("Updated: " + update );
+
+    }
+
+    @Test
+    public void e_delete() {
+
+        emp_service.delete( employee.getEmpID() );
+
+    }
 }
