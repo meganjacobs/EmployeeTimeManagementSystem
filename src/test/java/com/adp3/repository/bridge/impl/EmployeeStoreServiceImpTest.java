c5e3dd863917ee536d74ce2d1e64d8e4de626613
Manyati-2020 <manymunya@gmail.com>
2020-09-21 09:50:02 +0200

    Update and repair EmployeeStore

--- src/test/java/com/adp3/repository/bridge/impl/EmployeeStoreServiceImpTest.java
+++ src/test/java/com/adp3/repository/bridge/impl/EmployeeStoreServiceImpTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class EmployeeStoreRepositoryImpTest {
+public class EmployeeStoreServiceImpTest {
 
     @Test
     public void getAll() {
