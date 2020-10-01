ada7c5803337e7db7f6ce5ded25f3b72359a45d5
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2020-10-01 12:48:28 +0200

    Update

--- src/test/java/com/adp3/controller/standalone/LeaveControllerTest.java
+++ src/test/java/com/adp3/controller/standalone/LeaveControllerTest.java
@@ -1,67 +1,28 @@
 package com.adp3.controller.standalone;
 
-import com.adp3.entity.reports.LeaveReport;
-import com.adp3.entity.standalone.Leave;
-import com.adp3.factory.reports.LeaveReportFactory;
-import com.adp3.factory.reports.LeaveReportFactoryTest;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.*;
 
 public class LeaveControllerTest {
-    private  LeaveReport leaveReport = LeaveReportFactory.buildLeaveReport("");
 
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-    private  String baseUrl = "http://localhost:8080/leaveReporting/";
-    private LeaveReport employee =  LeaveReportFactory.buildLeaveReport("");
     @Test
     public void create() {
     }
 
     @Test
     public void read() {
-        String url = baseUrl + "read" + leaveReport.getLeaveReportID();
-        System.out.println("Store Read URL:  " + url);
-        ResponseEntity<Leave> leaveResponseEntity = restTemplate.getForEntity(url, Leave.class);
-        assertNotNull(leaveResponseEntity);
-        assertNotNull(leaveResponseEntity.getBody());
-
     }
 
     @Test
     public void update() {
-       /* Leave updated  = new Leave.Builder().copy(leaveReport).setLeaveDaysAmt(123456).setLeaveDesc("").setLeaveID("").build();
-        String url = baseUrl + "update";
-         ResponseEntity<Leave> storeResponseEntity = restTemplate.postForEntity(url,updated, Leave.class);
-        assertNotNull(storeResponseEntity);
-        assertNotNull(updated);
-
-    */}
-
-
-
+    }
 
     @Test
     public void delete() {
-        String url = baseUrl + "delete/" + leaveReport.getLeaveReportID();
-        restTemplate.delete(url);
     }
 
     @Test
     public void getAll() {
-        String url = baseUrl + "getAll" ;
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        assertNotNull(responseEntity);
     }
 }
