7e1e99893a82f599d1320125718edfb4171dec2b
Megan Jacobs <megan.jacobs@outlook.com>
2020-10-30 17:56:51 +0200

    pom.xml must include maven-compiler-plugin for LAMBDA expression to be used

--- src/test/java/com/adp3/service/reports/impl/LeaveReportServiceImplTest.java
+++ src/test/java/com/adp3/service/reports/impl/LeaveReportServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LeaveReportServiceImplTest {
 
-    private static LeaveReportService leaveReportService= LeaveReportServiceImpl.getService();
+    private static LeaveReportService leaveReportService;
     private static LeaveReport leaveReport = LeaveReportFactory.buildLeaveReport("Original request");
 
     @Test
