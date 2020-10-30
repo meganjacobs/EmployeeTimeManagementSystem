0c5365e7ed161df389df8324add2427315638e01
Megan Jacobs <megan.jacobs@outlook.com>
2020-10-30 17:09:47 +0200

    LeaveReportRepository annotated accordingly

--- src/main/java/com/adp3/repository/reports/LeaveReportRepository.java
+++ src/main/java/com/adp3/repository/reports/LeaveReportRepository.java
@@ -3,6 +3,7 @@ package com.adp3.repository.reports;
 import com.adp3.entity.reports.LeaveReport;
 import com.adp3.repository.IRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 /**
@@ -11,7 +12,7 @@ import java.util.Set;
  * Student number: 211137162
  * Description: LeaveReportRepository - extends member methods of parent "IRepository"
  */
-
+@Repository
 public interface LeaveReportRepository extends JpaRepository<LeaveReport,String> {
 
 }
