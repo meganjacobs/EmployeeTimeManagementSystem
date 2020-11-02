2f78e66273656a8281014ba920a7134b5500b442
Megan Jacobs <megan.jacobs@outlook.com>
2020-11-02 20:36:57 +0200

    LeaveReportRepostitory remove unused imports

--- src/main/java/com/adp3/repository/reports/LeaveReportRepository.java
+++ src/main/java/com/adp3/repository/reports/LeaveReportRepository.java
@@ -1,7 +1,6 @@
 package com.adp3.repository.reports;
 
 import com.adp3.entity.reports.LeaveReport;
-import com.adp3.repository.IRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,7 @@ import java.util.Set;
  * Author: Megan Jacobs
  * Class: Part Time
  * Student number: 211137162
- * Description: LeaveReportRepository - extends member methods of parent "IRepository"
+ * Description: LeaveReportRepository - extends JpaRepository framework incl. interface and implementations
  */
 @Repository
 public interface LeaveReportRepository extends JpaRepository<LeaveReport,String> {
