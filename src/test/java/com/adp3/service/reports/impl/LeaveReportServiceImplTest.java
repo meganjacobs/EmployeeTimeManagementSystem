e39ad1032d3d69208e880152a5f72b4566baa33e
Megan Jacobs <megan.jacobs@outlook.com>
2020-11-03 12:19:50 +0200

    LeaveReportServiceImpl logic moved out of test into impl class

--- src/test/java/com/adp3/service/reports/impl/LeaveReportServiceImplTest.java
+++ src/test/java/com/adp3/service/reports/impl/LeaveReportServiceImplTest.java
@@ -2,8 +2,6 @@ package com.adp3.service.reports.impl;
 
 import com.adp3.entity.reports.LeaveReport;
 import com.adp3.factory.reports.LeaveReportFactory;
-import com.adp3.service.reports.LeaveReportService;
-import org.hibernate.Session;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -29,50 +27,52 @@ import org.springframework.util.Assert;
 public class LeaveReportServiceImplTest {
 
     @Autowired
-    LeaveReportService leaveReportService;
+    LeaveReportServiceImpl leaveReportService;
     LeaveReport leaveReport;
 
     @Before
     public void setup() {
-        leaveReport = LeaveReportFactory.buildLeaveReport("emp001", "001", "cpt001");
+        leaveReport = LeaveReportFactory.buildLeaveReport("emp003", "003", "cpt003");
         leaveReportService.create(leaveReport);
     }
 
     @Test
     public void a_create() {
-        LeaveReport created = leaveReportService.create(leaveReport);
-        System.out.println("Created : " + created);
-        Assert.notNull(created, ("Created : " + created));
+        System.out.println("Created : " + leaveReport);
+        Assert.notNull(leaveReport, "values created and stored");
     }
+
+
 
     @Test
     public void b_read() {
         LeaveReport read = leaveReportService.read(leaveReport.getLeaveReportID());
         System.out.println("Read : " + read);
+        Assert.hasText(leaveReport.getEmpID(),"values read");
     }
 
     @Test
     public void c_update() {
-        LeaveReport updated = new LeaveReport.Builder()
-                .copy(leaveReport)
-                .setEmpID("emp002")
-                .setLeaveID("002")
-                .setStoreID("cpt002")
-                .build();
-        updated = leaveReportService.update(updated);
-        System.out.println("Updated : " + updated);
+        if (leaveReportService.read(leaveReport.getLeaveReportID())!=null){
+        leaveReportService.update(leaveReport);
+            System.out.println("Record Exists:  " + leaveReport.getLeaveReportID());
+        System.out.println("Updated : " + leaveReport);
+        }
+        else System.out.println("leaveReport does not exist in database:  ");
+        Assert.notNull(leaveReport);
+
     }
 
     @Test
     public void e_delete() {
         leaveReportService.delete(leaveReport.getLeaveReportID());
-        Assert.hasText(leaveReport.getEmpID(),("Deleted :" + leaveReport.getLeaveReportID()));
+        Assert.hasText(leaveReport.getEmpID(),"value existed and was deleted");
         System.out.println(("Deleted leaveReportID: " + leaveReport.getLeaveReportID()));
     }
 
     @Test
     public void d_getAll() {
-        Assert.notEmpty(leaveReportService.getAll(),"GetAll : " + leaveReportService.getAll());
+        Assert.notEmpty(leaveReportService.getAll(),"Set is empty");
         System.out.println("GetAll : " + leaveReportService.getAll());
     }
 }
