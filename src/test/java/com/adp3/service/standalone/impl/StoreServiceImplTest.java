478b2fb4d75b4d7032455499f79d2df008658c07
RiyaadAWS <riyaadryklief92@gmail.com>
2020-11-03 18:52:41 +0200

    updated Service test

--- src/test/java/com/adp3/service/standalone/impl/StoreServiceImplTest.java
+++ src/test/java/com/adp3/service/standalone/impl/StoreServiceImplTest.java
@@ -6,8 +6,12 @@ import com.adp3.service.standalone.StoreService;
 import com.adp3.util.GenericHelper;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 import java.util.Set;
 
@@ -20,12 +24,15 @@ import static org.junit.Assert.*;
  * Description: StoreServiceTest - this class tests the methods in StoreServiceImpl"
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class StoreServiceImplTest {
 
     @Autowired
-    private static StoreService service;
-    private static String storeID = GenericHelper.generateID();
-    private static Store store = StoreFactory.createStore("Cape Town");
+    StoreService service;
+    //Store store;
+    String storeID = GenericHelper.generateID();
+    Store store = StoreFactory.createStore("Cape Town");
 
     @Test
     public void d_getAll() {
@@ -36,9 +43,9 @@ public class StoreServiceImplTest {
 
     @Test
     public void a_create() {
-        Store created = service.create(store);
-        assertEquals(store.getStoreID(), created.getStoreID());
-        System.out.println("CREATED: " + created);
+        service.create(store);
+        assertEquals(store.getStoreID(), store.getStoreID());
+        System.out.println("CREATED: " + store);
     }
 
     @Test
@@ -49,13 +56,22 @@ public class StoreServiceImplTest {
 
     @Test
     public void c_update() {
-        Store updated = new Store.Builder()
+
+/*        Store updated = new Store.Builder()
                 .copy(store)
                 .setStoreID(storeID)
                 .setStoreName("Durban")
                 .build();
         updated = service.update(updated);
-        System.out.println("UPDATED: " + updated);
+        System.out.println("UPDATED: " + updated);*/
+
+        if (service.read(store.getStoreID())!=null){
+            service.update(store);
+            System.out.println("Record Exists:  " + store.getStoreID());
+            System.out.println("Updated : " + store);
+        }
+        else System.out.println("leaveReport does not exist in database:  ");
+        Assert.notNull(store);
     }
 
     @Test
