06be5ba60ac882fc2a98e113607b8e5207b7a99b
anongxa <61239936+anongxa@users.noreply.github.com>
2020-11-03 19:38:09 +0200

    DB Connection

---
+++ src/main/java/com/adp3/entity/Demography/Race.java
@@ -0,0 +1,40 @@
+package com.adp3.entity.Demography;
+
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Race {
+
+    @Id
+    private String id,
+    private String description;
+
+    private Race(){}
+
+    private Race(Builder builder){
+        this.id = builder.id;
+        this.description = builder.description;
+    }
+
+    public String getId() { return id; }
+
+    public String getDescription() { return description; }
+
+    @Override
+    public String toString() {
+        return "Race{" +
+                "id='" + id + '\'' +
+                ", description='" + description + '\'' +
+    }
+
+    public static class Builder {
+        private String id, description;
+
+        public Gender.Builder id(String id) {
+            this.id = id;
+            return this;
+    }
+}
