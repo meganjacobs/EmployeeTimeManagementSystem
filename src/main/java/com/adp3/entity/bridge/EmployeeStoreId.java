2d14546b269865c08e944ea3c66346cb72d2714c
Manyati-2020 <manymunya@gmail.com>
2020-11-05 09:19:00 +0200

    Changes 2

--- src/main/java/com/adp3/entity/bridge/EmployeeStoreId.java
+++ src/main/java/com/adp3/entity/bridge/EmployeeStoreId.java
@@ -3,14 +3,14 @@ package com.adp3.entity.bridge;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class EmployeeStoreId implements Serializable {
-    private String empID, storeID;
+public class EmployeeStoreID implements Serializable {
+private String empID,storeID;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        EmployeeStoreId that = (EmployeeStoreId) o;
+        EmployeeStoreID that = (EmployeeStoreID) o;
         return empID.equals(that.empID) &&
                 storeID.equals(that.storeID);
     }
