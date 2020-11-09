7847ced86914bbaca671003eb590be788318d1bd
Megan Jacobs <megan.jacobs@outlook.com>
2020-11-09 20:00:29 +0200

    LeaveReport: Security Application

--- src/test/java/com/adp3/controller/reports/LeaveReportControllerTest.java
+++ src/test/java/com/adp3/controller/reports/LeaveReportControllerTest.java
@@ -1,7 +1,14 @@
 package com.adp3.controller.reports;
 
+import com.adp3.entity.bridge.EmployeeLeave;
+import com.adp3.entity.bridge.EmployeeStore;
 import com.adp3.entity.reports.LeaveReport;
+import com.adp3.factory.bridge.EmployeeLeaveFactory;
+import com.adp3.factory.bridge.EmployeeStoreFactory;
 import com.adp3.factory.reports.LeaveReportFactory;
+import com.adp3.service.bridge.impl.EmpLeaveServiceImpl;
+import com.adp3.service.bridge.impl.EmployeeStoreServiceImpl;
+import com.adp3.service.standalone.impl.EmployeeServiceImpl;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -12,6 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -28,27 +37,37 @@ import static org.junit.Assert.*;
 public class LeaveReportControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    TestRestTemplate restTemplate;
+    @Autowired
+    EmpLeaveServiceImpl empLeaveService;
+    @Autowired
+    EmployeeStoreServiceImpl employeeStoreService;
     String baseURL="http://localhost:8080/leaveReport/";
     private ResponseEntity leaveReportResponseEntity = null;
     LeaveReport leaveReport;
 
     @Before
     public void setup() {
+
         leaveReport = LeaveReportFactory.buildLeaveReport("emp001", "001", "cpt001");
+        EmployeeLeave employeeLeave = EmployeeLeaveFactory.calcEmployeeLeave(leaveReport.getEmpID(),leaveReport.getLeaveID(),new Date(),new Date());
+        EmployeeStore employeeStore = EmployeeStoreFactory.createEmployeeStore(leaveReport.getEmpID(),leaveReport.getStoreID());
+        empLeaveService.create(employeeLeave);
+        employeeStoreService.create(employeeStore);
     }
     @Test
     public void a_create(){ // Test PostMapping
+
          leaveReportResponseEntity =
-                restTemplate.postForEntity(baseURL + "create", MediaType.APPLICATION_JSON,LeaveReport.class);
-        assertNotNull(leaveReportResponseEntity.getBody());
+                restTemplate.withBasicAuth("Super", "Password.ADP3").postForEntity(baseURL + "create/", leaveReport,LeaveReport.class);
+        //assertNotNull(leaveReportResponseEntity.getBody());
         System.out.println("POST LeaveReport : " + leaveReportResponseEntity.getBody());
     }
 
     @Test
     public void b_read() { // Test GetMapping
          leaveReportResponseEntity =
-                restTemplate.getForEntity(baseURL + "read/"+ leaveReport.getLeaveReportID(), LeaveReport.class);
+                restTemplate.withBasicAuth("User", "Password").getForEntity(baseURL + "read/"+ leaveReport.getLeaveReportID(), LeaveReport.class);
          System.out.println("GET LeaveReport : "+ leaveReport);
     }
 
@@ -65,14 +84,14 @@ public class LeaveReportControllerTest {
                     .setStoreID("cpt009")
                     .build();
 
-        restTemplate.put(baseURL + "update/" + "updated", updated);
+        restTemplate.withBasicAuth("Super", "Password.ADP3").put(baseURL + "update/" + "updated", updated);
             assertNotNull(updated);
         System.out.println("PUT LeaveReport : " + updated); }
     }
 
     @Test
     public void e_delete() { // Test DeleteMapping
-        restTemplate.delete(baseURL + "delete/" + leaveReport.getLeaveReportID());
+        restTemplate.withBasicAuth("Super", "Password.ADP3").delete(baseURL + "delete/" + leaveReport.getLeaveReportID());
         System.out.println("DELETED LeaveReport :  " +leaveReport.getLeaveReportID());
     }
 
@@ -80,7 +99,7 @@ public class LeaveReportControllerTest {
     public void d_getAll() {
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<LeaveReport> entity = new HttpEntity<>(null, headers);
-        ResponseEntity<String> result = restTemplate/*.withBasicAuth("Megan", "Password")*/
+        ResponseEntity<String> result = restTemplate.withBasicAuth("User", "Password")
                 .exchange(baseURL + "getAll/", HttpMethod.GET,entity, String.class);
             System.out.println("GETall LeaveReport: " + result.getBody());
     }
