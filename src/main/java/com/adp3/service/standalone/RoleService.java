5885f82ca22094a962548c55a999a77c58dc86f6
Megan Jacobs <Megan.Jacobs@outlook.com>
2020-11-19 08:19:29 +0200

    undo delete TimeKeepingServiceImpl

--- src/main/java/com/adp3/service/standalone/RoleService.java
+++ src/main/java/com/adp3/service/standalone/RoleService.java
@@ -2,62 +2,15 @@ package com.adp3.service.standalone.impl;
 
 import com.adp3.entity.standalone.Role;
 import com.adp3.repository.standalone.RoleRepository;
-import com.adp3.service.standalone.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-//import com.adp3.service.user.RoleService;
+public interface RoleService {
+    RoleRepository getAll();
 
-//import com.adp3.repository.standalone.Imp.RoleImp;
+    Role create(Role roleC);
 
-/* Name: Sanele Ngwenya
-        Student No.: 216019699
-        Group: PT*/
-@Service
-public class RoleServiceImp implements RoleService {
-    @Autowired
-    //private static RoleRepository repository = null;
-    private RoleRepository role;
+    Role read(String roleR);
 
+    Role update(Role roleD);
 
-//    private RoleServiceImp(){ this.repository = RoleImp.getRepository(); }
-//
-//    public static RoleServiceImp getService(){
-//        RoleServiceImp service = null;
-//        service = new RoleServiceImp();
-//        return service;
-//    }
-
-    @Override
-    public Set<Role> getAll() {
-
-        return role.findAll().stream().collect(Collectors.toSet());
-    }
-
-    @Override
-    public Role create(Role role) {
-        return this.role.save(role);
-    }
-
-    @Override
-    public Role read(String s) {
-
-        return this.role.findById(s).orElse(null);
-    }
-
-    @Override
-    public Role update(Role D) {
-        if (this.role.existsById(D.getRoleID())) {
-            return this.role.save(D);
-        }
-        return null;
-
-    }
-
-    @Override
-    public void delete(String sDelete) {
-        this.role.delete(role.getOne(sDelete));
-    }
+    void delete(String sDelete);
 }
